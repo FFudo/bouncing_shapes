4f3f9210409dd7caa37f73e65bd0f996f73d0e3b
FFudo <philipphofbauer@aon.at>
2025-01-25 22:49:25 +0100

    added random shapes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::{color::palettes::css::RED, prelude::*};
+use bevy::prelude::*;
+use rand::Rng;
+
+const MAX_SHAPES: i32 = 15;
 
 fn main() {
     App::new()
@@ -7,10 +10,53 @@ fn main() {
         .run();
 }
 
-#[derive(Component)] struct Player;
+#[derive(Component)]
+struct Shape;
 
-fn setup(mut materials: ResMut<Assets<ColorMaterial>>, mut meshes: ResMut<Assets<Mesh>>, mut commands: Commands) {
+fn setup(
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut commands: Commands,
+    window_query: Query<&Window>,
+) {
+    let mut rng = rand::thread_rng();
+
+    let width: f32;
+    if let Ok(window) = window_query.get_single() {
+        width = window.width();
+    } else {
+        width = 1.0;
+    }
+
     commands.spawn(Camera2d::default());
-    commands.spawn((Player, Mesh2d(meshes.add(Circle::new(50.0))), MeshMaterial2d(materials.add(ColorMaterial::from_color(RED)))));
-}
 
+    let mut shapes = Vec::new();
+
+    for _ in 0..MAX_SHAPES {
+        match rng.gen_range(0..3) {
+            0 => {
+                shapes.push(meshes.add(Circle::new(50.0)));
+            }
+            1 => {
+                shapes.push(meshes.add(Rectangle::new(50.0, 100.0)));
+            }
+            2 => {
+                shapes.push(meshes.add(Annulus::new(25.0, 50.0)));
+            }
+            _ => {
+                shapes.push(meshes.add(Rhombus::new(75.0, 100.0)));
+            }
+        }
+    }
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        let color = Color::hsl(360. * i as f32 / MAX_SHAPES as f32, 0.95, 0.7);
+
+        commands.spawn((
+            Mesh2d(shape),
+            MeshMaterial2d(materials.add(color)),
+            Transform::from_xyz(50. - (width / 2 as f32) + (i * 120) as f32, 0.0, 0.0),
+            Shape,
+        ));
+    }
+}
